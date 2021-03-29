745a7a2daac7856d4333c01ed3aa819d564590bf
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-03-29 15:40:07 +0300

    Create basic layout and Navigation component

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-container>
+    <el-header>
+      <Navigation />
+    </el-header>
+
+    <el-main>
+      <Nuxt />
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+import Navigation from "../components/main/Navigation"
+
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1>Work!</h1>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
