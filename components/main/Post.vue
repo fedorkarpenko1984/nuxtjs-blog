9426eece02183feb5968115ef7873b88e3a64af9
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-03-29 23:06:39 +0300

    Create route Post and update component Post.vue

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,18 +1,65 @@
 <template>
-  <article class="post">
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post"
+  >
+    <header slot="header" class="post-header">
+      <h3>Post title</h3>
+      <small>
+        <i class="el-icon-time"></i>
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
 
-  </article>
+    <div class="post-body">
+      <img
+        src="https://lumiere-a.akamaihd.net/v1/images/huang-hai-poster-new-swcom-slide-mobile-_2_ed8a046b.jpeg?region=0,0,1024,626&width=960"
+        alt="post image"
+        class="post-image"
+      >
+    </div>
+
+    <footer class="post-footer">
+      <el-button round type="primary" @click="openPost">Открыть</el-button>
+
+      <span>
+        <i class="el-icon-chat-dot-square"></i>
+        12
+      </span>
+    </footer>
+
+  </el-card>
 </template>
 
 <script>
 export default {
+  methods: {
+    openPost() {
 
+    }
+  }
 }
 </script>
 
 <style lang="sass" scoped>
   .post
-    height: 200px
-    border: 1px solid darkgray
-    margin-bottom: 2rem
+    margin-bottom: 1.5rem
+
+  .post-header
+    display: flex
+    justify-content: space-between
+    align-items: center
+
+  .post-image
+    width: 100%
+    height: auto
+
+  .post-footer
+    padding: 1rem
+    display: flex
+    justify-content: space-between
+    align-items: center
+
+
 </style>
