285f200267582113d4161f5f9e1a09dd698e8a31
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-03-29 16:00:13 +0300

    add styles for Navigation component

--- components/main/Navigation.vue
+++ components/main/Navigation.vue
@@ -1,13 +1,21 @@
 <template>
-
+  <nav>
+    <h1>Hi there</h1>
+  </nav>
 </template>
 
 <script>
 export default {
-name: "Navigation"
+
 }
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
+  nav
+    display: flex
+    align-items: center
+    justify-content: center
+    height: 100%
+    border-bottom: 1px solid #e6e6e6
 
 </style>
