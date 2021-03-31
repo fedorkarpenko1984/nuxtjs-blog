5565b45f959315047e59b70bf2503735ea4ae012
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-03-31 18:10:05 +0300

    Create layout Administration.vue and new route admin/index.vue in directiry `pages`

---
+++ layouts/administration.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>
+      <nuxt/>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
