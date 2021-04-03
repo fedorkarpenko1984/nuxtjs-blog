9dffca488e15c3c8c42c4c00434c023373737906
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-04-04 01:32:40 +0300

    Create conection to MongoDB and update application

--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,10 @@
 module.exports = {
-
+  MONGO_URI: `mongodb+srv://fedor:chakras88612@cluster0.ds0sl.mongodb.net/express-nuxt-app?retryWrites=true&w=majority`,
+  options: {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    writeConcern: {
+      j: true
+    }
+  }
 }
