6adfdeb2b558e32b95179020a065dcad6723d108
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-04-04 00:35:33 +0300

    Add app.js for server aplication.Add keys.dev.js and keys.prod.js for server config

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
-const app = express()
+const app = require('./app.js')
 
-// Import and Set Nuxt.js options
-let config = require('../nuxt.config.js')
+
+const config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 
 async function start() {
-  // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
   const {
@@ -26,10 +25,13 @@ async function start() {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
+  app.listen(port, host, () => {
+    consola.ready({
+      message: `Server listening on http://${host}:${port}`,
+      badge: true
+    })
   })
+
 }
+
 start()
