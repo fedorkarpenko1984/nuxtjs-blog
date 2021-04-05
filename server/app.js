d632ffbc6a87d61bfcd4202d591133e9f9e63679
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-04-06 00:43:13 +0300

    Add JS library `passport`. Add passport strategy for authication

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const passportStrategy = require('./middleware/passport-strategy')
 const authRoutes = require('./routes/auth.routes')
 const keys = require('./keys/index')
 const app = express()
@@ -8,6 +10,9 @@ const app = express()
 mongoose.connect(keys.MONGO_URI, keys.options)
   .then(() => console.log('MongoDB connected...'))
   .catch(error => console.log(error))
+
+app.use(passport.initialize())
+passport.use(passportStrategy)
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
