da159f07327e508c48e3697d09b32ba56649bfb4
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-04-07 21:57:17 +0300

    Add functional to `post.controller.js`

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,25 +1,70 @@
 const Post = require('../models/post.model')
 
 module.exports.createPost = async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    text: req.body.text,
+    imageUrl: `/${req.file.filename}`
+  })
 
+  try {
+    await post.save()
+    res.status(201).json(post)
+  } catch (e) {
+    res.status(500).json(e)
+  }
 }
 
 module.exports.getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().sort({date: -1})
+    res.json(posts)
+  } catch (e) {
+    res.status(500).json(e)
+  }
 
 }
 
 module.exports.getById = async (req, res) => {
-
+  try {
+    await Post.findById(req.params.id).populate('comments').exec((error, post) => {
+      res.json(post)
+    })
+  } catch (e) {
+    res.status(500).json(e)
+  }
 }
 
 module.exports.updatePost = async (req, res) => {
-
+  const $set = {
+    text: req.body.text
+  }
+  try {
+    const post = Post.findOneAndUpdate({
+      _id: req.params.id
+    }, {$set}, {new: true})
+  } catch (e) {
+    res.status(500).json(e)
+  }
 }
 
 module.exports.removePost = async (req, res) => {
-
+  try {
+    await Post.deleteOne({_id: req.params.id})
+    res.json({message: 'Пост удален'})
+  } catch (e) {
+    res.status(500).json(e)
+  }
 }
 
 module.exports.addView = async (req, res) => {
-
+  const $set = {
+    views: ++req.body.views
+  }
+  try {
+    await Post.findOneAndUpdate({_id: req.params.id}, {$set})
+    res.status(204).json()
+  } catch (e) {
+    res.status(500).json(e)
+  }
 }
